3892a81db20c6ac759e8f4d1c73fc02082aaab11
Justin Forgue <juforgue@gmail.com>
2019-12-18 01:51:26 -0500

    require jquery and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
+/ This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -7,8 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap/dist/js/bootstrap")
 
+var jQuery = require("jquery")
+
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('bootstrap');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,10 +22,3 @@ require("bootstrap/dist/js/bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'bootstrap'
-import './src/application.scss'
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
