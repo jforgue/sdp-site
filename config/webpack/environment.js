914bd6391eab6011f2b3b85afc93747a24762440
Justin Forgue <juforgue@gmail.com>
2019-12-18 01:49:00 -0500

    prepend jquery plugins

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,13 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack');
+const webpack = require('webpack')
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}));
-
-environment.config.set('resolve.alias', {jquery: 'jquery/src/jquery'});
-
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': "tether",
+    Popper: ['popper.js', 'default'], // for Bootstrap 4
+  })
+)
 module.exports = environment
