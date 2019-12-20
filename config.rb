d3af9565cf8617ff7733d9758885901f9802baf6
jforgue <juforgue@gmail.com>
2019-12-19 22:55:50 -0500

    forgot the nodes

--- config.rb
+++ config.rb
@@ -1,49 +1,20 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+require 'pathname'
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
-
-# Layouts
-# https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+webpack_build = './node_modules/webpack/bin/webpack.js --bail'
+webpack_watch = './node_modules/webpack/bin/webpack.js --watch -d'
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ? webpack_build : webpack_watch,
+  source: '.tmp/dist',
+  latency: 1
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
-
-activate :directory_indexes
-
+::SassC.load_paths.concat [Pathname(__dir__).join('node_modules')]
